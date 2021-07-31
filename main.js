7565f6ed002b0fb3e813f52c3f5f024c200cab92
Candy Crush <76581343+Aadya-ai@users.noreply.github.com>
2021-07-31 12:58:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function first() {
+    window.location.href = "https://aadya-ai.github.io/object-detection-p2/";
+}
+
+function second() {
+    window.location.href = "https://aadya-ai.github.io/object-detection-p3/";
+}
+
+function third() {
+    window.location.href = "https://aadya-ai.github.io/object-detection-p4/";
+}
+
+function fourth() {
+    window.location.href = "https://aadya-ai.github.io/object-detection-p5/";
+}
+
+function Kchat() {
+    window.location.href = "https://aadya-ai.github.io/OBJECT_DETECTION_ON_IMAGES-Kchat/";
+}
